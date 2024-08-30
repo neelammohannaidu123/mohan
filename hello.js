9b999fe7d8b63cf4bafcb61729f1272c102ff341
mohan <neelammohannaidu123@gmail.com>
2024-08-30 09:21:27 +0530

    changes

---
+++ hello.js
@@ -0,0 +1,4 @@
+console.log('sachin')
+console.log('hello 1234')
+console.log('naidu')
+console.log('about file')
---
+++ index.js
@@ -0,0 +1,3 @@
+console.log('sachin')
+console.log('hello 123')
+console.log('mohan')
